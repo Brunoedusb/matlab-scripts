f8bc8a11db348165419ae5bc2e1061f01ccfca66
homelist <brunoed@ucl.br>
2018-09-13 18:20:20 -0300

    first commit

---
+++ raiz.m
@@ -0,0 +1,17 @@
+%Escreva uma função que calcule a raiz quadrada do parâmetro de entrada com um erro máximo
+%de 0,001. 
+
+disp ('Função que calcula a raiz quadrada');
+
+%Pega o valor de entrada
+n = input (' Entre com um valor:');
+
+%Calcula a raiz quadrada e joga na variável de saída com um erro máximo
+%de 0,001
+fatorCalculo = 0.001;
+while fatorCalculo * fatorCalculo < n
+    fatorCalculo = fatorCalculo + 0.001;
+end 
+
+%Imprime o valor da variável saida
+fprintf( 'A raiz quadrada é = %6.2f \n', fatorCalculo);
